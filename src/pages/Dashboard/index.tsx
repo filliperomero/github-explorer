557c6f8480386d77f06028a6a959612bc0e7b530
filliperomero <fillipe.romero@gmail.com>
2020-05-21 16:03:28 -0300

    chore: add repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import logo from '../../assets/logo.svg';
@@ -67,7 +68,7 @@ const Dashboard: React.FC = () => {
         <input
           placeholder="Digite o nome do reposiório"
           value={newRepo}
-          onChange={(e) => setNewRepo(e.target.value)}
+          onChange={e => setNewRepo(e.target.value)}
         />
         <button type="submit">Pesquisar</button>
       </Form>
@@ -75,8 +76,11 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
-        {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -87,7 +91,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} color="cbcbd6" />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
